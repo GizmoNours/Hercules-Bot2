c94f62c57d3f201007fbe7479e46122770b217ba
GizmoNours <82664620+GizmoNours@users.noreply.github.com>
2021-04-16 19:24:05 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -99,8 +99,7 @@ client.on("message", msg => {
 });
 
 let port = process.env.PORT || 5000; 
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+app.listen(PORT, () => {   
 })
 
 client.login(process.env.TOKEN);
