e3274b990c2b8710e83fcd67c7aff46f45e6ce61
GizmoNours <82664620+GizmoNours@users.noreply.github.com>
2021-04-16 18:58:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ client.on("message", msg => {
 var express = require('express');
 var app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
