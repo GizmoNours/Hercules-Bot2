b11d0a070a0b51b9f4bfe5027eda5a3dddf9887f
GizmoNours <82664620+GizmoNours@users.noreply.github.com>
2021-04-23 02:51:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 require('events').EventEmitter.defaultMaxListeners = 0
 
