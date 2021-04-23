2a4516e6bd93809fa4e301ff4ed39f0757e04b82
GizmoNours <82664620+GizmoNours@users.noreply.github.com>
2021-04-23 02:18:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -122,9 +122,9 @@ client.on("message", msg => {
     }}
 });
 
-let port = process.env.PORT || 5000;
+http.createServer(onRequest).listen(process.env.PORT || 5000)
+//let port = process.env.PORT || 5000;
 //client.listen(process.env.PORT, '0.0.0.0')
-
 
 client.login(process.env.TOKEN);
 
